d71202184f7c63140ccc59a0fce3256036ebd266
HonzaBoh <honza.boh00@gmail.com>
2023-02-26 15:01:22 +0100

    Guess who's dumb and missed commit all in one

--- Player.java
+++ Player.java
@@ -1,5 +1,9 @@
 package gui.maze;
 
+/**
+ * Nothing of notice here, possibly I would suggest some constructor or encapsulaiton
+ * as to prevent null pointers
+ */
 public class Player {
     Tile currentLocation;
     boolean holdingOrb;
